8924b99e42b9733abc67a9213d8c05f419f621a1
blueglassblock@outlook.com <blueglassblock@outlook.com>
2020-11-14 10:14:33 +0800

    Class edit: Mirai.Session(wrapper of original session parser) and Process(multiprocess function calling)

--- utils/source.py
+++ utils/source.py
@@ -1,6 +1,8 @@
+import importlib
 import json
+import multiprocessing
 import os
-import threading
+import sys
 
 import requests
 import ruamel.yaml as yaml
@@ -23,32 +25,43 @@ class Mirai:
         self.websockets_ip = self.config_dict['Websockets_IP']
         yaml_fs.close()
 
-    def gen_session(self):
-        """
-        Generate & verify a Mirai Session
-        return format:
-        {
-        'sessionKey': 'ExampleString',
-        'qq': 1234567890
-        }
-        """
+    class Session:
+        def __init__(self, mirai_inst):
+            """
+            Automatically generates session
+            """
+            self.session_dict = self.gen()
+            self.session_key = self.session_dict['session']
+            self.qq = self.session_dict['qq']
+            self.mirai_inst = mirai_inst
 
-        reply = requests.post(self.httpip + '/auth', data=json.dumps({"authKey": self.config_dict['authKey']}))
-        reply_dict = json.loads(bytes.decode(reply.content))
-        session = {"sessionKey": reply_dict['session'], "qq": self.config_dict['qq']}
-        requests.post(self.httpip + '/verify', data=json.dumps(session))
-        return session  # return session and qq in dict form
+        def gen(self):
+            """
+                    Generate & verify a Mirai Session
+                    return format:
+                    {
+                    'sessionKey': 'ExampleString',
+                    'qq': 1234567890
+                    }
+                    """
 
-    def release_session(self, session):
-        """
-        release a Mirai Session
-        :return: nothing
-        """
-        requests.post(self.httpip + '/release', data=json.dumps(session))
+            reply = requests.post(self.mirai_inst.httpip + '/auth',
+                                  data=json.dumps({"authKey": self.mirai_inst.config_dict['authKey']}))
+            reply_dict = json.loads(reply.text)
+            session = {"sessionKey": reply_dict['session'], "qq": self.mirai_inst.config_dict['qq']}
+            requests.post(self.mirai_inst.httpip + '/verify', data=json.dumps(session))
+            return session  # return session and qq in dict form
+
+        def release(self):
+            """
+                    release a Mirai Session
+                    :return: nothing
+                    """
+            requests.post(self.mirai_inst.httpip + '/release', data=json.dumps(self.session_dict))
 
     async def websockets_receive(self, session: dict):
-        uri = self.websockets_ip + '/all?sessionKey=' + session['sessionKey']
-        with websockets.connect(uri) as current_ws_conn:
+        url = self.websockets_ip + '/all?sessionKey=' + session['sessionKey']
+        with websockets.connect(url) as current_ws_conn:
             message_dict = json.loads(await current_ws_conn.recv())
         return message_dict
 
@@ -81,33 +94,58 @@ class Mirai:
         reply = requests.post(uri, send_data)
         msg_id = -1
         try:
-            reply_dict = json.loads(bytes.decode(reply.content))
+            reply_dict = json.loads(reply.text)
             msg_id = reply_dict['messageId']
         finally:
             return msg_id
 
 
 class MessageParser:
+    def __init__(self, mirai_inst, process_inst):
+        self.mirai_inst = mirai_inst
+        self.process_inst = process_inst
+
     def command_parse(self, content: dict):  # TODO: Implement stop all process
         if content['args'][0] == 'stop':
-            raise NotImplementedError()
+            sys.exit()
+        try:
+            Process.proc_call(self.process_inst, content['args'][0], content['args'][1], content['args'][2:])
+        except NotImplementedError():
+            print("Not Implemented\n")
         return
 
     def console_input(self, using: bool):
         while using:
             text = input()
-            command_tuple = text.split()
+            command_list: list = text.split()
             content_dict = {
                 'sender': 'console',
                 'content': text,
-                'args': command_tuple
+                'args': command_list
             }
             self.command_parse(content_dict)
         return
 
 
-def start_thread(func: callable, args: tuple, name=None):
-    thread = threading.Thread(target=func, args=args, name=name)
-    thread.setDaemon(True)
-    thread.start()
-    return thread
+class Process:
+    def __init__(self):
+        self.pool = multiprocessing.Pool()
+
+    def proc_call(self, mod: str, func: str, args: list):
+        """
+        Call a function
+        :param mod:
+        :param func:
+        :param args:
+        :return:
+        """
+        module = importlib.import_module(mod)
+        caller = getattr(module, func)
+        self.pool.apply_async(caller, args)
+        return
+
+    def shut(self):
+        """
+        Closes the pool
+        """
+        self.pool.close()
