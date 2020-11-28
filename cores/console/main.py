80ca66afabca3f291ab1451c9e7f7da9236311db
blueglassblock@outlook.com <blueglassblock@outlook.com>
2020-11-28 14:07:15 +0800

    Log utils

--- cores/console/main.py
+++ cores/console/main.py
@@ -1,21 +1,23 @@
+import importlib
 import os
 import sys
 import time
 
-sys.path.append('.')
+import utils
+
 if __name__ == '__main__':
+    log_const = importlib.import_module('log')
+    log_path = log_const.log_path
+    logger = utils.Logger('CONSOLE', 'debug', log_path)
     if os.path.exists('./stop.lck'):
         os.remove('./stop.lck')
     while True:
         input_str = input()
         if input_str == 'stop':
+            logger.log('info', 'Stopping PyBot...')
             stop_file_path = os.path.abspath('./stop.lck')
-            open(stop_file_path, 'w')
+            stop_fs = open(stop_file_path, 'w')
+            stop_fs.close()
             time.sleep(0.5)
             os.remove(stop_file_path)
             sys.exit()
-        else:
-            try:
-                eval(input_str)
-            except Exception as e:
-                print(e)
